873970a957841ada8c1b3b7a0ba2ce10ba7a03de
Anthony-Rodriguez <7Anthony.Rodriguez7@gmail.com>
2021-06-07 15:52:41 -0400

    Rewrote hooks to fetch data for cards

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import './App.css';
-import axios from 'axios'
-import { useState } from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Navbar from './components/Sidebar/Sidebar.js'
 import Home from './routes/landingPage.js'
@@ -9,15 +7,6 @@ import Decks from './routes/decks.js'
 import Cards from './routes/cards.js'
 
 function App() {
-  const [cards, setCards] = useState()
-  axios.get('https://db.ygoprodeck.com/api/v7/cardinfo.php')
-    .then((response) => {
-      console.log(response);
-      setCards({cards: response.data})
-    })
-   .catch((error)=>{
-      console.log(error);
-   });
   return (
     <>
     <Router>
@@ -26,7 +15,7 @@ function App() {
           <Route path='/' exact component={Home} />
           <Route path='/account' exact component={Account} />
           <Route path='/decks' exact component={Decks} />
-          <Route path='/cards' exact component={Cards} cards={cards}/>
+          <Route path='/cards' exact component={Cards}/>
         </Switch>
     </Router>
     </>
--- src/routes/cards.js
+++ src/routes/cards.js
@@ -1,11 +1,56 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
+function Cards() {
+  let [cards, setCards] = useState(null)
+  let [cardJsx, setCardJsx] = useState(null)
+  
+  useEffect(() => {
+      axios({
+        url: 'https://db.ygoprodeck.com/api/v7/cardinfo.php',
+        method: "GET"
+      })
+      .then((res) => {
+        setCards({cards: res.data})
+      })
+     .catch((error)=> {
+        console.log(error);
+     })
+  }, []);
 
-export default function Cards(cards) {
-  return (
+  useEffect(() => {
+    if (cards === null || cards === undefined) {
+      setCardJsx('Loading...')
+    } else if (cards.length === 0) {
+      setCardJsx("The cards are gone.")
+    } else {
+      console.log(cards)
+      const cardList = cards.map(card => {
+        return (
+          <div key={card.id} className="game-card">
+            <div>{card.name}</div>
+            <div>{card.type}</div>
+            <img alt={card.name} src={card.card_images.image_url}/>
+            <div>{card.atk}</div>
+            <div>{card.def}</div>
+            <div>{card.level}</div>
+            <div>{card.race}</div>
+            <div>{card.attribute}</div>
+          </div>
+        )
+      })
+      setCardJsx(cardList)
+    }
+  }, [cards])
+
+  return cardJsx ? (
     <div className='cards'>
       <h1>Cards</h1>
-      <div className={cards.type}></div>
+      <div>
+        {cardJsx}
+      </div>
     </div>
-  )
+  ) : ( <h1>Cards</h1> )
 }
+
+export default Cards
